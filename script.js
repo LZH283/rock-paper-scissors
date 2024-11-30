6094d354c391608a22bf6f702315f79a1f94ae72
LZH <168682669+LZH283@users.noreply.github.com>
2024-11-30 14:38:40 -0800

    made playable rock paper scissors game

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
+
+let humanChoice;
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(max = 3){
     // generates a random number 0, 1 or 2
     let choiceNumber = Math.floor(Math.random() * max);
     if (choiceNumber === 0){
-        choiceString = "Rock"
+        choiceString = "rock"
     } else if (choiceNumber === 1){
-        choiceString = "Paper"
+        choiceString = "paper"
     } else {
-        choiceString = "Scissors"
+        choiceString = "scissors"
+    }
+}
+
+function getHumanChoice (){
+    humanChoice = prompt("What do you pick?").toLowerCase();
+}
+
+function playRound (humanChoice, choiceString){
+    if (humanChoice === choiceString){
+        console.log("Its a tie!")
+    } 
+    else if (humanChoice == "rock" && choiceString == "paper"){
+        console.log("You lose!")
+    } else if (humanChoice == "rock" && choiceString == "scissors"){
+        console.log("You win!")
+    } 
+      else if (humanChoice == "paper" && choiceString == "scissors"){
+        console.log("You lose!")
+    } else if (humanChoice == "paper" && choiceString == "rock"){
+        console.log("You win!")
+    }
+      else if (humanChoice == "scissors" && choiceString == "rock"){
+        console.log("You lose!")
+    } else if (humanChoice == "scissors" && choiceString == "rock"){
+        console.log("You win!")
+    } else {
+        console.log("error")
     }
 }
 
 
 getComputerChoice()
-console.log(choiceString)
+console.log(choiceString);
+
+getHumanChoice()
+console.log(humanChoice);
+
+playRound(humanChoice, choiceString)
+
